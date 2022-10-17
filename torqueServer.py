68d9805e4ad9a3a6f1f3c6f4e5256ce76898a3eb
cmdviegas <cmdviegas@users.noreply.github.com>
2022-10-17 11:49:47 -0300

    modified:   torqueServer.py 	modified:   torqueCodes.json

--- torqueServer.py
+++ torqueServer.py
@@ -8,58 +8,70 @@
 # SCRIPT TO READ DATA FROM TORQUE/OBD2
 #
 
-# libs
-from socket import * 
 from datetime import datetime
-import csv
-import netifaces as ni
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import urllib.parse
+import sys
+import json
+import socket
 
-# get ip address
-for intf in ni.interfaces():
-    if intf == 'lo':
-        continue
-    else:
-        serverIPaddr = ni.ifaddresses(intf)[ni.AF_INET][0]['addr'] # get server IP address
-        break
+class TorqueApp(BaseHTTPRequestHandler):
 
-# socker creation
-serverPort = 5000 # port
-serverSocket = socket(AF_INET,SOCK_STREAM) # create socket
-serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # reuse address
-serverSocket.bind(('',serverPort)) # bind socket to port
-serverSocket.listen(1) # listen for connections
+    counter = 0
 
-def getCode(key): # get code from key
-    with open('codes.csv', 'r') as inputFile:
-        csv_file = csv.reader(inputFile)
-        for row in csv_file:
-            if row[0] == key:
-                return row[1]
+    def timeStamp(self):
+        return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
-print ('> servidor iniciado %s:%d ...' % (serverIPaddr,serverPort))
-lineCounter = 1
-try:
-  while 1:
-    dateTime = datetime.now().strftime("%d/%m/%Y %H:%M:%S") # get current date and time
-    clientSocket, clientAddr = serverSocket.accept() # wait for client connection
-    inputData = clientSocket.recv(1024).decode('utf-8')  # read data from client
-    outputData = inputData.split("&") # split data into a list
-    lenoutputData = len(outputData) # get number of elements in the list
-    outputData[lenoutputData-1] = outputData[lenoutputData-1].split(' ')[0] # remove the HTTP/1.0 from the last element
-    print ('%d [%s] > %s:%d' % (lineCounter, dateTime, clientAddr[0], clientAddr[1])) # print date, client IP and port
-    print('\t\t\t > %s' % outputData) # print data based on codes.csv
-    #for row in outputData[1:]: # print data
-    #    rowParts = row.split('=') # split data into a list
-    #    #if rowParts[0] == 'time':
-    #    #  rowParts[1] = str(datetime.fromtimestamp(166518417990))
-    #    print('\t\t\t > %s = %s' % (getCode(rowParts[0]), rowParts[1])) # print data based on codes.csv
-    response = "HTTP/1.0 200 OK\r\n\r\n" # HTTP response
-    #clientSocket.send(response.encode('utf-8')) # send HTTP response
-    lineCounter = lineCounter + 1 # increment line counter
-    clientSocket.close() # close client connection
-except KeyboardInterrupt:
-  print("Caught keyboard interrupt, exiting") # print message when user press CTRL+C
-finally:
-  serverSocket.close() # close server socket
+    def do_GET(self):
+        # Parse out the GET URL parameters
+        params = urllib.parse.parse_qs(urllib.parse.urlparse(self.path).query)
+        TorqueApp.counter += 1
 
-  datetime.fromtimestamp(1485714600).strftime("%A, %B %d, %Y %I:%M:%S")
+        print(
+            self.timeStamp(), 
+            f'Message {TorqueApp.counter} from {self.client_address}'
+            )
+
+        for key,value in params.items():
+            if key == 'eml' or key == 'time' or key == 'session' or key == 'v' or key == 'id':
+                # ignore email address and other already used fields
+                continue
+            elif value[0] == 'NaN':
+                # Skip invalid data. This is likely a divide by zero error.
+                continue
+            elif value[0] == 'Infinity' or value[0] == '-Infinity':
+                # Skip invalid data. This is likely a divide by zero error.
+                continue
+            else:
+                #print(self.getJsonByKey(key))
+                print('\t > %s = %s' % (self.getJsonByKey(key), value[0])) # print data based on torqueCodes.json
+
+        self.send_response_only(200)
+        self.end_headers()
+        self.wfile.write(bytes('OK!', 'UTF-8'))
+
+    def getJsonByKey(self, key):
+        with open('torqueCodes.json') as json_file:
+            torqueCodes = json.load(json_file)
+            for code in torqueCodes:
+                if code['id'] == key:
+                    return code['description']
+
+if __name__ == '__main__':
+    serverPort = 5000 # port
+    serverHost = '' # host
+    httpd = HTTPServer((serverHost, serverPort), TorqueApp)
+    host, port = httpd.socket.getsockname()[:2]
+    print(
+        TorqueApp.timeStamp(0), 
+        f'Server starts - {host}:{port}'
+        )
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        httpd.server_close()
+        print(
+            TorqueApp.timeStamp(0), 
+            f'Server stops - {host}:{port}'
+            )
+        sys.exit(0)
